a1a099e37409dd906a7b31b21ff1bff4f4cfaeab
E-Jhay Bumacod <bumacodejhay@gmail.com>
2022-01-03 14:06:28 +0800

    add laravel back up

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('order:statusUpdate')->everyMinute();
+        $schedule->command('backup:run')->quarterly();
     }
 
     /**
